a1858df5bc94fb206317f3847523afc60768030a
Iqbal Setiawan <ibal.setiawan08@gmail.com>
2024-02-28 20:23:06 +0700

    add loading

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import React, { useState, useEffect } from "react";
 import Header from "./pages/Header";
 import MainSection from "./pages/MainSection";
 import Footer from "./pages/Footer";
+import Loading from "./components/Loading";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
     <>
       <Header />
